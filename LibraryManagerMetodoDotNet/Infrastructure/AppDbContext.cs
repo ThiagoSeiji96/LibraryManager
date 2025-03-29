32cef96919f46789f869e8e7f8389dd4f8840d8a
Thiago Seiji Imai Rozner <thiago.rozner@viveo.com.br>
2025-03-29 14:48:34 -0300

    Add: config banco de dados e models

---
+++ LibraryManagerMetodoDotNet/Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagerMetodoDotNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerMetodoDotNet.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseInMemoryDatabase("LivrosDb");
+        }
+    }
+}
---
+++ LibraryManagerMetodoDotNet/Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerMetodoDotNet.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string AnoPublicacao { get; set; }
+
+    }
+}
