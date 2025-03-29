7e2a5d69e5be6b6d12503dec79bfaf51d1212530
Thiago Seiji Imai Rozner <thiago.rozner@viveo.com.br>
2025-03-29 14:54:25 -0300

    add: lógica de execução do programa e commandos

---
+++ LibraryManagerMetodoDotNet/Commands/LivroCommand.cs
@@ -0,0 +1,51 @@
+﻿using LibraryManagerMetodoDotNet.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagerMetodoDotNet.Commands
+{
+    public static class LivroCommand
+    {
+        public static void AdicionarLivro(LivroService livroService)
+        {
+            Console.Write("Digite o título do livro: ");
+            string titulo = Console.ReadLine();
+            Console.Write("Digite o autor do livro: ");
+            string autor = Console.ReadLine();
+            Console.Write("Digite o ano de publicação: ");
+            string anoPublicacao = Console.ReadLine();
+
+            livroService.CriarLivro(titulo, autor, anoPublicacao);
+        }
+
+        public static void AtualizarLivro(LivroService livroService)
+        {
+            Console.Write("Digite o ID do livro que deseja atualizar: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Digite o novo título do livro: ");
+            string titulo = Console.ReadLine();
+            Console.Write("Digite o novo autor do livro: ");
+            string autor = Console.ReadLine();
+            Console.Write("Digite o novo ano de publicação: ");
+            string anoPublicacao = Console.ReadLine();
+
+            livroService.AtualizarLivro(id, titulo, autor, anoPublicacao);
+        }
+
+        public static void DeletarLivro(LivroService livroService)
+        {
+            Console.Write("Digite o ID do livro que deseja deletar: ");
+            string id = Console.ReadLine();
+
+            livroService.DeletarLivro(id);
+        }
+
+        public static void ListarLivros(LivroService livroService)
+        {
+            livroService.ListarLivros();
+        }
+    }
+}
--- LibraryManagerMetodoDotNet/Program.cs
+++ LibraryManagerMetodoDotNet/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using LibraryManagerMetodoDotNet.Commands;
+using LibraryManagerMetodoDotNet.Infrastructure;
+using LibraryManagerMetodoDotNet.Services;
+
+using (var context = new AppDbContext())
+{
+    var livroService = new LivroService(context);
+
+    // Exibe o menu até o usuário escolher sair
+    bool sair = false;
+    while (!sair)
+    {
+        Console.Clear();
+        Console.WriteLine("----- Menu -----");
+        Console.WriteLine("1. Adicionar Livro");
+        Console.WriteLine("2. Listar Livros");
+        Console.WriteLine("3. Atualizar Livro");
+        Console.WriteLine("4. Deletar Livro");
+        Console.WriteLine("5. Sair");
+        Console.Write("Escolha uma opção: ");
+        string opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+            case "1":
+                LivroCommand.AdicionarLivro(livroService);
+                break;
+            case "2":
+                LivroCommand.ListarLivros(livroService);
+                break;
+            case "3":
+                LivroCommand.AtualizarLivro(livroService);
+                break;
+            case "4":
+                LivroCommand.DeletarLivro(livroService);
+                break;
+            case "5":
+                sair = true;
+                break;
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+
+        if (!sair)
+        {
+            Console.WriteLine("Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+        }
+    }
+}
